4e27b48ec1b62663037bced3df90ca78fa34b93e
cqm655 <cqm655@gmail.com>
2022-11-16 12:57:18 +0200

    creating nodes with title from taxonomy

--- web/modules/custom/ib_task_93/src/Form/ConfigurationForm.php
+++ web/modules/custom/ib_task_93/src/Form/ConfigurationForm.php
@@ -2,10 +2,11 @@
 
 namespace Drupal\ib_task_93\Form;
 
-use Drupal\Core\CoreServiceProvider;
+use Drupal\node\Entity\Node;
 use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Render\Element\Value;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -14,17 +15,17 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class ConfigurationForm extends ConfigFormBase {
 
   /**
-   * Drupal\Core\Cache\CacheBackendInterface definition.
+   * Drupal\Core\Entity\EntityTypeManagerInterface definition.
    *
-   * @var Drupal\Core\Cache\CacheBackendInterface
+   * @var Drupal\Core\Entity\EntityTypeManagerInterface
    */
-  protected $drupalService;
+  protected $entityTypeManager;
 
   /**
    * {@inheritdoc}
    */
-  public function __construct(EntityTypeManager $drupal_service) {
-    $this->drupalService = $drupal_service;
+  public function __construct(EntityTypeManager $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
   }
 
   /**
@@ -57,20 +58,19 @@ class ConfigurationForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('ib_tassk_93s.content');
 
     $taxonomy_country = [];
 
-    $taxonomy_countries = $this->drupalService->getStorage("taxonomy_term")->loadTree('country');
-    foreach ($taxonomy_countries as $variable) {
-      $taxonomy_country[] = $variable->name;
+    $taxonomy_countries = $this->entityTypeManager->getStorage("taxonomy_term")->loadTree('country');
+    foreach ($taxonomy_countries as $country) {
+
+      $taxonomy_country[$country->tid] = $country->name;
     }
 
-    $form['countries'] = [
+    $form['country'] = [
       '#type' => 'select',
       '#options' => $taxonomy_country,
-      '#title' => $this->t('Countries'),
-      '#default_value' => $config->get('countries'),
+      '#title' => $this->t('Country'),
     ];
 
     return parent::buildForm($form, $form_state);
@@ -80,11 +80,23 @@ class ConfigurationForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    // Loading term name by tid from DB.
+    $term = $this->entityTypeManager->getStorage('taxonomy_term')->load($form_state->getValue('country'));
+
+    $term_country_name = $term->label();
+
     // Retrieve the configuration.
     $this->config('ib_tassk_93s.content')
-      // Set the submitted configuration setting.
-      ->set('countries', $form_state->getValue('countries'))
+    // Set the submitted configuration setting.
+      ->set('country', $form_state->getValue('country'))
       ->save();
+
+    // Create node and save.
+    $node_country = Node::create(['type' => 'article']);
+    $node_country->set('title', $term_country_name);
+    $node_country->enforceIsNew();
+    $node_country->save();
 
     parent::submitForm($form, $form_state);
 
