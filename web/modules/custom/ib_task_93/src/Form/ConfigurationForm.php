0eb5c93c6fa4541ab96a9e41aa39024e268317d3
cqm655 <cqm655@gmail.com>
2022-11-16 11:26:39 +0200

    added loading option from Taxonomy, DI for services

--- web/modules/custom/ib_task_93/src/Form/ConfigurationForm.php
+++ web/modules/custom/ib_task_93/src/Form/ConfigurationForm.php
@@ -2,13 +2,38 @@
 
 namespace Drupal\ib_task_93\Form;
 
+use Drupal\Core\CoreServiceProvider;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Configure example settings for this site.
  */
 class ConfigurationForm extends ConfigFormBase {
+
+  /**
+   * Drupal\Core\Cache\CacheBackendInterface definition.
+   *
+   * @var Drupal\Core\Cache\CacheBackendInterface
+   */
+  protected $drupalService;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(CoreServiceProvider $drupal_service) {
+    $this->drupalService = $drupal_service;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -33,15 +58,18 @@ class ConfigurationForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('ib_tassk_93s.content');
 
+    $taxonomy_country = [];
+
+    $taxonomy_countries = $this->drupalService->getStorage("taxonomy_term")->loadTree('country');
+    foreach ($taxonomy_countries as $variable) {
+      $taxonomy_country[] = $variable->name;
+    }
+
     $form['countries'] = [
       '#type' => 'select',
-      '#options' => ['Moldova', 'Belarus', 'Urkaine', 'Russia'],
+      '#options' => $taxonomy_country,
       '#title' => $this->t('Countries'),
       '#default_value' => $config->get('countries'),
-    ];
-
-    $form['show_country'] = [
-      '#weight' => 100,
     ];
 
     return parent::buildForm($form, $form_state);
@@ -55,7 +83,6 @@ class ConfigurationForm extends ConfigFormBase {
     $this->config('ib_tassk_93s.content')
       // Set the submitted configuration setting.
       ->set('countries', $form_state->getValue('countries'))
-      ->set('show_country', $form_state->getValue('countries'))
       ->save();
 
     parent::submitForm($form, $form_state);
